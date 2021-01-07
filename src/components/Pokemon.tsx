e70244368d1a6cb06dab11ad691f9c22cae791ac
MisterQuestions <misterquestions.mx@gmail.com>
2021-01-06 22:42:15 -0600

    fix: add redirect to pokemon info and clickable logo

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import createStyles from '@material-ui/core/styles/createStyles';
+import Link from 'next/link';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import PokemonResource from '@services/interfaces/pokemon/Pokemon';
+import { capitalize } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
       padding: theme.spacing(2),
+      cursor: 'pointer',
     },
     imageContainer: {
-      padding: theme.spacing(8),
       display: 'flex',
       alignItems: 'center',
     },
@@ -21,16 +23,18 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
-const Pokemon: React.FC<PokemonResource> = ({ sprites, name }) => {
+const Pokemon: React.FC<PokemonResource> = ({ id, sprites, name }) => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root} elevation={3}>
-      <div className={classes.imageContainer}>
-        <img className={classes.imageElement} src={sprites.other?.['official-artwork'].front_default} alt={name} />
-      </div>
-      <Typography>{name}</Typography>
-    </Card>
+    <Link href={`/pokemon/${id}`}>
+      <Card className={classes.root} elevation={3}>
+        <div className={classes.imageContainer}>
+          <img className={classes.imageElement} src={sprites.other?.['official-artwork'].front_default} alt={name} />
+        </div>
+        <Typography variant="subtitle1">{capitalize(name)}</Typography>
+      </Card>
+    </Link>
   );
 };
 
